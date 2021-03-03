a3c55d7e883d5d36ce17807170cbf596d6be2031
chesterher11206 <chester11206@gmail.com>
2021-03-03 08:21:38 +0800

    modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,18 @@ cd ~/NTU/semmed-label-system
 git checkout main
 git pull
 npm install
+npm run build
 
 if [[ ! $(pm2 pid LabelSystem) ]]
 then
     pm2 start npm --name LabelSystem -- start --watch
+else
+    pm2 restart LabelSystem
 fi
 
 if [[ ! $(pm2 pid ApiServer) ]]
 then
     pm2 start npm --name ApiServer -- run api --watch
+else
+    pm2 restart ApiServer
 fi
